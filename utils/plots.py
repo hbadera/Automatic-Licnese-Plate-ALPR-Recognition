ba93026d6e1ddacfaeac0e228fe3dde1b805205f
Himanshu Badera <62422827+hbadera@users.noreply.github.com>
2022-11-22 22:26:54 -0600

    Update plots.py

--- utils/plots.py
+++ utils/plots.py
@@ -483,3 +483,4 @@ def save_one_box(xyxy, im, file=Path('im.jpg'), gain=1.02, pad=10, square=False,
         f = str(increment_path(file).with_suffix('.jpg'))
         Image.fromarray(cv2.cvtColor(crop, cv2.COLOR_BGR2RGB)).save(f, quality=95, subsampling=0)
     return crop
+  .
