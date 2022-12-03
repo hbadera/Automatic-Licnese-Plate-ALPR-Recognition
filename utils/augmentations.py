72e5111a1a770aa26959cd3fc83f1b81a96d850d
Himanshu Badera <62422827+hbadera@users.noreply.github.com>
2022-12-03 01:23:19 -0600

    Update augmentations.py

--- utils/augmentations.py
+++ utils/augmentations.py
@@ -281,3 +281,5 @@ def box_candidates(box1, box2, wh_thr=2, ar_thr=100, area_thr=0.1, eps=1e-16):  
     w2, h2 = box2[2] - box2[0], box2[3] - box2[1]
     ar = np.maximum(w2 / (h2 + eps), h2 / (w2 + eps))  # aspect ratio
     return (w2 > wh_thr) & (h2 > wh_thr) & (w2 * h2 / (w1 * h1 + eps) > area_thr) & (ar < ar_thr)  # candidates
+
+.
